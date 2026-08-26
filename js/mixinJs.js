d3ea0b03650016ebbd31dad36f4b3891ada5bc69
yji35j42012 <yji35j42012@gmail.com>
2026-08-26 14:07:49 +0800

    修改mixin

--- js/mixinJs.js
+++ js/mixinJs.js
@@ -1,6 +1,6 @@
 
 
 var url =
-	"https://script.google.com/macros/s/AKfycbxKlnoQqSJw8mMiee0gHSvY-AVqm4Aa9EGIYk447IuUYx6kOsb4LZV1ADo4cmML9mHg/exec";
+	"https://script.google.com/macros/s/AKfycbzKWTJUSH9S3nfnhqQzC_-a7289AwNgdVxW56kw3Uis0oGAr8gK9YhzVD0HlYEJWoVe/exec";
 	
 
