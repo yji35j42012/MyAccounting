f152294e6ad9830e98819f6a10439dd97cdb5a3f
Alex <yji35j42012@gmail.com>
2026-09-17 17:50:40 +0800

    測試

--- js/quote-proxy-config.js
+++ js/quote-proxy-config.js
@@ -1,1 +1,1 @@
-window.CASHFLOW_QUOTE_PROXY_URL = "https://cashflow-yahoo-quotes.yji35j42020.workers.dev";
+// window.CASHFLOW_QUOTE_PROXY_URL = "https://cashflow-yahoo-quotes.yji35j42020.workers.dev";
