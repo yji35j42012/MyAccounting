208563cdc34d39118726e7551b88389f0b465bcf
Alex <yji35j42012@gmail.com>
2026-09-17 18:23:14 +0800

    測

--- js/quote-proxy-config.js
+++ js/quote-proxy-config.js
@@ -1,5 +1,5 @@
-// window.CASHFLOW_QUOTE_PROXY_URL = "https://cashflow-yahoo-quotes.yji35j42020.workers.dev";
-window.CASHFLOW_QUOTE_PROXY_URL = "https://cashflow-yahoo-quotes2.yji35j42020.workers.dev/";
+window.CASHFLOW_QUOTE_PROXY_URL = "https://cashflow-yahoo-quotes.yji35j42020.workers.dev";
+// window.CASHFLOW_QUOTE_PROXY_URL = "https://cashflow-yahoo-quotes2.yji35j42020.workers.dev/";
 
 
 
